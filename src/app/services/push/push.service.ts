da73d8bdfdefcf7f253b61c0b5cfa860542b5183
Jesse Mykolyn <jrmykolyn@gmail.com>
2019-09-22 11:52:53 -0400

    Update PushService: add support for retrieving subscription ids

--- src/app/services/push/push.service.ts
+++ src/app/services/push/push.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PushService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  getSubscriptions() {
+    // TODO: Move API URL into config.
+    return this.http.get('http://localhost:4600/subscriptions');
+  }
 }
