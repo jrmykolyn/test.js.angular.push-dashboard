c5d388db34c2c48dd980e213edf9b40d94371c6d
Jesse Mykolyn <jrmykolyn@gmail.com>
2019-09-22 11:53:16 -0400

    Update AppComponent to fetch and display subscriptions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PushService } from './services/push/push.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test.js.angular.push-dashboard';
+  subscriptions$ = this.pushService.getSubscriptions();
+
+  constructor(
+    private pushService: PushService
+  ) {}
 }
